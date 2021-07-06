607480b211acf50b3390c1591185ebd8a71ef258
Thiago A. M. Delatorre <thiago.delatorre@gmail.com>
2021-07-06 12:40:44 -0400

    Finish basic implementation

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
+const { exception } = require('console');
 var http = require('http');
 
 let storage = {};
+let nextId = {};
 
 http.createServer(function (req, res) {
-    paths = req.url.split('/')
-    collection = paths[1]
+    try {
+        paths = req.url.split('/')
+        collection = paths[1]
 
-    if (!(collection in storage)) {
-        storage[collection] = {}
-    }
+        if (!(collection in storage)) {
+            storage[collection] = {}
+            nextId[collection] = 0
+        }
 
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+        if (req.method == 'GET') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(Object.values(storage[collection])));
+        } else if (req.method == 'DELETE') {
+            id = +paths[2]
 
-    if (req.method == 'GET') {
-        res.end(JSON.stringify(Object.values(storage[collection])));
-    } else if (req.method == 'DELETE') {
-        id = +paths[2]
-
-        delete storage[collection][id];
-
-        res.end(JSON.stringify(id))
-    } else {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
-        });
-        req.on('end', () => {
-            let result = JSON.parse(body)
-
-            let id = 0;
-            if (req.method == 'POST') {
-                id = Object.keys(storage[collection]).length
-            } else if (req.method == 'PUT') {
-                id = +paths[2]
+            if (isNaN(id)) {
+                throw "id not specified."
             }
 
-            result.id = id;
-            storage[collection][id] = result
+            if (!(id in storage[collection])) {
+                throw "id doesn't exist."
+            }
 
-            res.end(JSON.stringify(result))
-        });
+            delete storage[collection][id];
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(id))
+        } else {
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString(); // convert Buffer to string
+            });
+            req.on('end', () => {
+                try {
+                    let result = JSON.parse(body)
+
+                    let id = 0;
+                    if (req.method == 'POST') {
+                        id = nextId[collection]
+                        nextId[collection]++;
+
+                    } else if (req.method == 'PUT') {
+                        id = +paths[2]
+
+                        if (isNaN(id)) {
+                            throw "id not specified."
+                        }
+
+                        if (!(id in storage[collection])) {
+                            throw "id doesn't exist."
+                        }
+
+                    }
+
+                    result.id = id;
+                    storage[collection][id] = result
+
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(result))
+                } catch (err) {
+
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(err))
+                }
+            });
+        }
+    } catch (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(err))
     }
 }).listen(8080);
