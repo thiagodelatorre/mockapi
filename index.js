38efaec24a4f9f53731f84fb7b9ed62b64df8680
Thiago A. M. Delatorre <thiago.delatorre@gmail.com>
2021-07-06 12:26:51 -0400

    Finish partial implementation of RESTful API

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ var http = require('http');
 let storage = {};
 
 http.createServer(function (req, res) {
+    paths = req.url.split('/')
+    collection = paths[1]
+
+    if (!(collection in storage)) {
+        storage[collection] = {}
+    }
+
+
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify([]));
+
+    if (req.method == 'GET') {
+        res.end(JSON.stringify(Object.values(storage[collection])));
+    } else if (req.method == 'DELETE') {
+        id = +paths[2]
+
+        delete storage[collection][id];
+
+        res.end(JSON.stringify(id))
+    } else {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', () => {
+            let result = JSON.parse(body)
+
+            let id = 0;
+            if (req.method == 'POST') {
+                id = Object.keys(storage[collection]).length
+            } else if (req.method == 'PUT') {
+                id = +paths[2]
+            }
+
+            result.id = id;
+            storage[collection][id] = result
+
+            res.end(JSON.stringify(result))
+        });
+    }
 }).listen(8080);
